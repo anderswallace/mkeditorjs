66fd61ec7bae00884ea1d6a549984979632acded
anderswallace <anderswallace20@gmail.com>
2025-01-18 16:34:40 -0600

    added horizontal resizing functionality and bug fixes

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from "react-resizable";
+import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./resizable.css";
 
 interface ResizableProps {
@@ -7,11 +7,28 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
-  return (
-    <ResizableBox height={300} width={Infinity} resizeHandles={["s"]}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+
+  if (direction === "horizontal") {
+    resizableProps = {
+      className: "resize-horizontal",
+      maxConstraints: [Math.floor(window.innerWidth * 0.75), Infinity],
+      minConstraints: [Math.floor(window.innerWidth * 0.2), Infinity],
+      height: Infinity,
+      width: Math.floor(window.innerWidth * 0.75),
+      resizeHandles: ["e"],
+    };
+  } else {
+    resizableProps = {
+      maxConstraints: [Infinity, Math.floor(window.innerHeight * 0.9)],
+      minConstraints: [Infinity, 50],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
