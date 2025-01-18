ec027453081bc3410672172407c2a125d0209002
anderswallace <anderswallace20@gmail.com>
2025-01-18 15:48:15 -0600

    added resizing functionality to code editor and preview windows

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import "./preview.css";
 
 interface PreviewProps {
   code: string;
@@ -37,12 +38,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
