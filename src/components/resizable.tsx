af712fba487928094b55eb5c47e066a7030beb2e
anderswallace <anderswallace20@gmail.com>
2025-01-18 18:33:16 -0600

    fixed width bugs when resizing window

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 import "./resizable.css";
 
@@ -8,19 +9,46 @@ interface ResizableProps {
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(Math.floor(window.innerWidth * 0.75));
+
+  useEffect(() => {
+    const listener = () => {
+      let timer: any;
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (Math.floor(window.innerWidth * 0.75) < width) {
+          setWidth(Math.floor(window.innerWidth * 0.75));
+        }
+      }, 100);
+    };
+    window.addEventListener("resize", listener);
+
+    return () => {
+      window.removeEventListener("resize", listener);
+    };
+  }, []);
 
   if (direction === "horizontal") {
     resizableProps = {
       className: "resize-horizontal",
-      maxConstraints: [Math.floor(window.innerWidth * 0.75), Infinity],
-      minConstraints: [Math.floor(window.innerWidth * 0.2), Infinity],
+      maxConstraints: [Math.floor(innerWidth * 0.75), Infinity],
+      minConstraints: [Math.floor(innerWidth * 0.2), Infinity],
       height: Infinity,
-      width: Math.floor(window.innerWidth * 0.75),
+      width,
       resizeHandles: ["e"],
+      onResizeStop: (event, data) => {
+        setWidth(data.size.width);
+      },
     };
   } else {
     resizableProps = {
-      maxConstraints: [Infinity, Math.floor(window.innerHeight * 0.9)],
+      maxConstraints: [Infinity, Math.floor(innerHeight * 0.9)],
       minConstraints: [Infinity, 50],
       height: 300,
       width: Infinity,
