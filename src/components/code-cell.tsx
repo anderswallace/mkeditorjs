a3a2aa7eae24b1aaa23dedfabdb44fc5332e4020
awallace2 <anderswallace20@gmail.com>
2025-01-19 17:15:23 -0600

    added additional handling for different error types

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -8,13 +8,15 @@ import Resizable from "./resizable";
 
 const CodeCell = () => {
   const [code, setCode] = useState("");
+  const [err, setErr] = useState("");
   const [input, setInput] = useState("");
 
   // debounce logic for automatic code execution
   useEffect(() => {
     const timer = setTimeout(async () => {
       const output = await bundle(input);
-      setCode(output);
+      setCode(output.code);
+      setErr(output.err);
     }, 1000);
 
     return () => {
@@ -31,7 +33,7 @@ const CodeCell = () => {
             onChange={(value) => setInput(value)}
           />
         </Resizable>
-        <Preview code={code} />
+        <Preview code={code} bundleError={err} />
       </div>
     </Resizable>
   );
