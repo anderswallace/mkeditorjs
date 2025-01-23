f2c277a5163d915d45e23cdcd4b52fe564736037
anderswallace <anderswallace20@gmail.com>
2025-01-23 17:19:42 -0600

    refactored components to use redux and added buttons to move cells

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,20 +1,26 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { useState, useEffect } from "react";
+import { useActions } from "../hooks/use-actions";
 
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
 import bundle from "../bundler";
 import Resizable from "./resizable";
+import { Cell } from "../state";
 
-const CodeCell = () => {
+interface CodeCellProps {
+  cell: Cell;
+}
+
+const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const [code, setCode] = useState("");
   const [err, setErr] = useState("");
-  const [input, setInput] = useState("");
+  const { updateCell } = useActions();
 
   // debounce logic for automatic code execution
   useEffect(() => {
     const timer = setTimeout(async () => {
-      const output = await bundle(input);
+      const output = await bundle(cell.content);
       setCode(output.code);
       setErr(output.err);
     }, 1000);
@@ -22,15 +28,15 @@ const CodeCell = () => {
     return () => {
       clearTimeout(timer);
     };
-  }, [input]);
+  }, [cell.content]);
 
   return (
     <Resizable direction="vertical">
       <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 1;"
-            onChange={(value) => setInput(value)}
+            initialValue={cell.content}
+            onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
         <Preview code={code} bundleError={err} />
